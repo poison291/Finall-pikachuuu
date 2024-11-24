afba69bbf0275f2cf2ef8cd8574f25a5d88de9a7
poison <shahsusan291@gmail.com>
2024-11-24 20:02:54 +0545

    switch to IDE #2

--- src/API/pokemon.jsx
+++ src/API/pokemon.jsx
@@ -10,7 +10,9 @@ export default function Pokemon() {
   <>
     <div className="bg-gray-800 h-lvh">
       <div className="">
+  <div className="">
 
+  </div>
         <div className="text-white">
       <h1>{pokemon.name}</h1>
         </div>
